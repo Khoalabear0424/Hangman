3d3c2771eb2dc48755179a62ac1b0a12def70ae2
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2018-12-18 18:33:06 -0800

    Fixed Wrong Guesses, Removed Button, Enter to run

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
-var word = "Harry Potter";
+var wordList = ["Harry Potter", "Ezri", "Iron Man", "Wonder Woman", "Hulk", "Yo Momma", "Miles Morales", "Superman", "Legolas", "El Chupacabra"];
+
+var random = Math.floor(Math.random()*wordList.length);
+var word = wordList[random];
+console.log(word);
 
 var wordArray = word.split("");
 var wordArrayBlank = word.split("");
@@ -17,20 +21,26 @@ document.getElementById("blank").innerHTML = wordArrayBlank.join(" ");
 
 //------------Reveals Correct Guesses---------//
 function guessFunction() {
+    
     var userGuess = document.getElementById("guess").value;
+    console.log(userGuess);
     var counter = 0;
 
     for (var x = 0; x < wordArray.length; x++) {
         if (userGuess.toUpperCase() == wordArray[x].toUpperCase()) {
             wordArrayBlank[x] = wordArray[x];
         }else{
+
+            //--------Collects Wrong Guesses------//
             counter ++;
-            if(counter == wordArray.length){
+            if(counter == wordArray.length && !wrongGuesses.includes(userGuess)){
                 wrongGuesses.push(userGuess);
+                
             }
         }
     }
-    document.getElementById("wrong").innerHTML = "Incorrect Guesses: " + wrongGuesses.join(", ");
+    document.getElementById("guess").value = "";
+    document.getElementById("wrong").innerHTML = "Incorrect Guesses: " + wrongGuesses.join(" ");
     document.getElementById("blank").innerHTML = wordArrayBlank.join(" ");
 }
 
