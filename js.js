05e35a8b24e64f71d4f4cd7e2239754a1bb431ed
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2018-12-24 13:55:57 -0800

    Added colored buttons on press

--- js.js
+++ js.js
@@ -1,12 +1,16 @@
-var wordList = ["Harry Potter", "Ezri", "Iron Man", "Wonder Woman", "Hulk", "Yo Momma", "Miles Morales", "Superman", "Legolas", "El Chupacabra"];
 
-var random = Math.floor(Math.random()*wordList.length);
+
+//-----------Create Word List-------------//
+wordList = ["Harry Potter", "Ezri", "Iron Man", "Wonder Woman", "Hulk", "Yo Momma", "Miles Morales", "Superman", "Legolas", "El Chupacabra"];
+
+var random = Math.floor(Math.random() * wordList.length);
 var word = wordList[random];
 console.log(word);
 
 var wordArray = word.split("");
 var wordArrayBlank = word.split("");
-var wrongGuesses =[];
+var wrongGuesses = [];
+
 
 //-----------Display Blank Words-----------//
 for (var x = 0; x < wordArrayBlank.length; x++) {
@@ -19,28 +23,37 @@ for (var x = 0; x < wordArrayBlank.length; x++) {
 document.getElementById("blank").innerHTML = wordArrayBlank.join(" ");
 
 
+
 //------------Reveals Correct Guesses---------//
-function guessFunction() {
-    
-    var userGuess = document.getElementById("guess").value;
-    console.log(userGuess);
+function guessFunction(letter) {
+
+    var userGuess = letter.getAttribute('value');
     var counter = 0;
 
     for (var x = 0; x < wordArray.length; x++) {
         if (userGuess.toUpperCase() == wordArray[x].toUpperCase()) {
             wordArrayBlank[x] = wordArray[x];
-        }else{
-
+            $("button[value=" + userGuess + "]").css({
+                "color":"white",
+                "background-color":"green"
+            });
+        } else {
             //--------Collects Wrong Guesses------//
-            counter ++;
-            if(counter == wordArray.length && !wrongGuesses.includes(userGuess)){
+            counter++;
+            if (counter == wordArray.length && !wrongGuesses.includes(userGuess)) {
                 wrongGuesses.push(userGuess);
-                
             }
         }
     }
-    document.getElementById("guess").value = "";
+//-------Colors Red to Wrong Guess
+    if(counter==wordArray.length){
+        $("button[value=" + userGuess + "]").css({
+            "color":"white",
+            "background-color":"red"
+        });
+    }
     document.getElementById("wrong").innerHTML = "Incorrect Guesses: " + wrongGuesses.join(" ");
     document.getElementById("blank").innerHTML = wordArrayBlank.join(" ");
 }
+
 
