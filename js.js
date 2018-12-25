01eff6dafd7c89bd7b7bdb38e2a9be1fce791b94
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2018-12-24 17:44:16 -0800

    Random img and name

--- js.js
+++ js.js
@@ -5,26 +5,32 @@ wordList = ["Spiderman", "Venom", "Iron Man", "Hawk Eye", "Hulk", "Black Panther
 
 //-------------Img List-----------------//
 imgList = [
-    "https://www.syfy.com/sites/syfy/files/wire/legacy/34038.jpg",
-    "https://www.gannett-cdn.com/presto/2018/10/03/USAT/bb930017-108b-47cc-9d0b-47a5eefb734f-GW-PUBLICITY-00187_r.jpg?crop=5999,3374,x1,y62&width=3200&height=1680&fit=bounds",
-    "https://i.ytimg.com/vi/yKHeEOoMw64/maxresdefault.jpg",
-    "https://thecineasteslament.files.wordpress.com/2011/12/jeremy-renner.jpg",
-    "https://dceptiongaming.files.wordpress.com/2016/02/1414472485.jpg",
-    "https://media.vanityfair.com/photos/5a7a33609b38580e8603ca37/master/w_768,c_limit/Black-Panther.jpg",
-    "http://images.fandango.com/images/fandangoblog/Marvel_AntMan-150601.jpg",
-    "https://data.whicdn.com/images/136232434/original.jpg",
-    "https://cdn-static.denofgeek.com/sites/denofgeek/files/2016/02/deadpool_face.jpg",
-    "https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/10/1489149452-wolverine-surprise-hugh-jackman-wants-to-be-wolverine-forever-and-here-s-how-he-can-do-it.jpeg?resize=480:*",
-    "https://static1.srcdn.com/wordpress/wp-content/uploads/X-Men-Apocalypse-Cyclops-Tye-Sheridan-Hi-Res1.jpg",
-    "https://i.amz.mshcdn.com/Wm09wUKlO6fa7l4jH0gDP3zhCkk=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F209004%2F24136897352_c5d0ac2fa3_o.jpg"
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
+    "8.png",
+    "9.png",
+    "10.png",
+    "11.png",
+    "12.png"
 ]
 
-console.log(imgList[0]);
-
-
 var random = Math.floor(Math.random() * wordList.length);
+var basePath = "./img/";
+
+var image = document.getElementById("photo");
+image.src=basePath+imgList[random];
+
+
+
 var word = wordList[random];
 console.log(word);
+
+
 
 var wordArray = word.split("");
 var wordArrayBlank = word.split("");
