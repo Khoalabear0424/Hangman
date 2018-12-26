59e5f0bf1fa0443d475593f7e2d22918e9b18cee
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2018-12-25 21:19:19 -0800

    added Fadein new image

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
 
 wordList = [
-    "Spiderman", 
-    "Venom", 
-    "Iron Man", 
-    "Hawk Eye", 
-    "Hulk", 
-    "Black Panther", 
-    "Ant Man", 
-    "Black Widow", 
-    "Dead Pool", 
-    "Wolverine", 
-    "Cyclops", 
+    "Spiderman",
+    "Venom",
+    "Iron Man",
+    "Hawk Eye",
+    "Hulk",
+    "Black Panther",
+    "Ant Man",
+    "Black Widow",
+    "Dead Pool",
+    "Wolverine",
+    "Cyclops",
     "Phoenix"
 ];
 imgList = [
@@ -27,7 +27,7 @@ imgList = [
     "11.png",
     "12.png"
 ];
-captionList = [
+captionListHint = [
     "A student, a photographer, and your friendly neighborhood hero.",
     "A failed photographer, he's not Peter Parker's biggest fan.",
     "He's gonna die in Avengers Endgame.",
@@ -41,14 +41,28 @@ captionList = [
     "Stare deeply into his eyes, and you'll die.",
     "A truly tragic hero."
 ]
+captionListAnswer = [
+    "I don't feel so good...",
+    "WE ARE VENOM.",
+    "R.I.P.",
+    "I'm out of arrows!",
+    "Don't make him angry!",
+    "I never freeze.",
+    "ANT-hony!!",
+    "Beautiful, but deadly..",
+    "*Fourth Wall Breaking*",
+    "*SNIKT!*",
+    "Everything is always red.",
+    "KILL EVERYONE!!"
+]
 var random = Math.floor(Math.random() * wordList.length);
-var basePath = "./img/";
+var basePath = "./img/alterEgo/";
 
 var image = document.getElementById("photo");
-image.src=basePath+imgList[random];
+image.src = basePath + imgList[random];
 
 var cap = document.getElementById("caption");
-cap.innerHTML=captionList[random];
+cap.innerHTML = captionListHint[random];
 
 var word = wordList[random];
 console.log(word);
@@ -72,9 +86,9 @@ document.getElementById("blank").innerHTML = wordArrayBlank.join(" ");
 
 
 //------------Add Event Listner to Buttons--------//
-for (var x = 0; x<26; x++){
+for (var x = 0; x < 26; x++) {
     var butt = document.querySelectorAll("button");
-    butt[x].addEventListener("click", function(){
+    butt[x].addEventListener("click", function () {
         guessFunction(this);
     });
 }
@@ -90,9 +104,9 @@ function guessFunction(letter) {
         if (userGuess.toUpperCase() == wordArray[x].toUpperCase()) {
             wordArrayBlank[x] = wordArray[x];
             $("button[value=" + userGuess + "]").css({
-                "color":"yellow",
-                "background-color":"red",
-                "font-weight":"bolder"
+                "color": "yellow",
+                "background-color": "red",
+                "font-weight": "bolder"
             });
         } else {
             //--------Collects Wrong Guesses------//
@@ -104,23 +118,41 @@ function guessFunction(letter) {
             //^^ THis was used to display incorrect guesses but since i added red backgrounds to incorrect guesses, dont need this anymore
         }
     }
+
     
-    //-------Colors Red to Wrong Guess
-    if(counter==wordArray.length){
-        $("button[value=" + userGuess + "]").css({
-            "color":"gray",
-            "background-color":"black"
+    //--------------Win State---------------//
+    if (!wordArrayBlank.includes(" __ ")) {
+        console.log("WIN!")
+        var secondPath = "./img/hero/";
+
+        $("#photo").fadeOut(
+            function () {
+                $(this).attr('src', secondPath + imgList[random]).fadeIn(1000);
+        });
+
+        $("#caption").fadeOut(
+            function () {
+                $(this).html(captionListAnswer[random]).fadeIn(1000);
         });
     }
-    
-    function wait(ms){
+
+    //-------Color Wrong Guess
+    if (counter == wordArray.length) {
+        $("button[value=" + userGuess + "]").css({
+            "color": "purple",
+            "background-color": "black"
+        });
+    }
+
+    function wait(ms) {
         var start = new Date().getTime();
         var end = start;
-        while(end < start + ms) {
+        while (end < start + ms) {
             end = new Date().getTime();
         }
     }
-    
+
     document.getElementById("blank").innerHTML = wordArrayBlank.join(" ");
 }
+
 
